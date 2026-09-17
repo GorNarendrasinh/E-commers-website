9c41d9d8987c71be95bd6d6a6833f2085808cf7d
Gor Narendrasinh <narendrasinhgor94@gmail.com>
2026-09-17 12:21:46 +0530

    Update settings.py

--- ecommers/settings.py
+++ ecommers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rifict53$phusglud3n+q4$=2$+64u48)7zrc*o@b8jyhc48@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
